1ff10a7c438d4b57b7208ff09159a869eb06f895
Tex M. <164884889+Ranchhand87@users.noreply.github.com>
2025-03-05 11:22:47 -0700

    Update docusaurus.config.ts

--- contributor-beginner/docusaurus.config.ts
+++ contributor-beginner/docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://intersectmbo.github.io/developer-experience/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/developer-experience/contributor-beginner',
+  baseUrl: '/developer-experience/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
