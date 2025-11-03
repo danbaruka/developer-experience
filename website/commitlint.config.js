fea28da018359842db1b523561261ede7d677ff9
danbaruka <danamphred@gmail.com>
2025-11-03 14:31:43 +0400

    feat: make changelog test

---
+++ website/commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // New feature
+        'fix', // Bug fix
+        'docs', // Documentation changes
+        'style', // Code style changes (formatting, etc.)
+        'refactor', // Code refactoring
+        'perf', // Performance improvements
+        'test', // Adding or updating tests
+        'build', // Build system or dependencies
+        'ci', // CI/CD changes
+        'chore', // Other changes that don't modify src or test files
+        'revert', // Revert a previous commit
+      ],
+    ],
+    'subject-case': [0], // Allow any case in subject
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+  },
+};
+
